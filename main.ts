613f23fe6effa306650dcdc0d6499f6d1f435d5f
ryanjrman <rftheappmaker@gmail.com>
2024-11-18 13:27:58 -0700

    Added the ability to shift down

--- main.ts
+++ main.ts
@@ -12,32 +12,61 @@ export default class Blocks extends Plugin { // The main plugin class
     async onload() { // Things that happen on plugin load
         console.log('Blocks Plugin Loaded') //REMOVE
 
-        this.addCommand({
+        this.addCommand({ // Adds a command to the command pallete
             id: 'move-current-line-up',
             name: 'Move current line up',
-            editorCallback: (editor: Editor, view: MarkdownView) => {
-                const line: number = editor.getCursor().line; // Gets the current line number the cursor is on
-                const aboveLine: number = line - 1; // Gets the above line's number
-                const lineText: string = editor.getLine(line); // Gets the text on the line the cursor is on
-                const aboveLineText: string = editor.getLine(aboveLine); // Gets the text on the above line
-                const cursorPos: EditorPosition = editor.getCursor(); // Gets the cursor position
-                cursorPos.line = cursorPos.line - 1; // Updates the cursor position to be on the line the current line shifts to
-                
-                if(line && aboveLine) { // Checks if there is a line the cursor is on and a line above
-                    editor.setLine(line, aboveLineText); // Place the above line on the current line
-                    editor.setLine(aboveLine, lineText); // Place the current line on the above line
-                    editor.setCursor(cursorPos); // Sets the cursor at its new position on the above line
-                } else {
-                    return;
-                }
+            editorCallback: (editor: Editor, view: MarkdownView) => { // Checks to see if there is an editor active before running the command
+                this.shiftLine(true, editor, view);
+            }
+        });
+
+        this.addCommand({ // Adds a command to the command pallete
+            id: 'move-current-line-down',
+            name: 'Move current line down',
+            editorCallback: (editor: Editor, view: MarkdownView) => { // Checks to see if there is an editor active before running the command
+                this.shiftLine(false, editor, view);
             }
         });
     }
 
     async onunload() { // Things that will be unloaded when the plugin is disabled
         console.log('Blocks Plugin Unoaded') //REMOVE
+    }   
+
+    shiftLine(isUp: boolean, editor: Editor, view: MarkdownView) { // Shifts a line into a different position
+        const line: number = editor.getCursor().line; // Gets the current line number the cursor is on
+        const lineText: string = editor.getLine(line); // Gets the text on the line the cursor is on
+        const cursorPos: EditorPosition = editor.getCursor(); // Gets the cursor position
+
+        if (isUp) { 
+            const aboveLine: number = line - 1; // Gets the above line's number
+
+            if (line && aboveLine) { // Checks if there is a line the cursor is on and a line above
+                cursorPos.line = cursorPos.line - 1; // Updates the cursor position to be on the line the current line shifts to
+                const aboveLineText: string = editor.getLine(aboveLine); // Gets the text on the above line
+
+                editor.setLine(line, aboveLineText); // Place the above line on the current line
+                editor.setLine(aboveLine, lineText); // Place the current line on the above line
+                editor.setCursor(cursorPos); // Sets the cursor at its new position on the above line
+            } else {
+                return;
+            }
+        } else {
+            const belowLine: number = line + 1; // Gets the below line's number
+
+            if (line && belowLine) { // Checks if there is a line the cursor is on and a line below
+                cursorPos.line = cursorPos.line + 1; // Updates the cursor position to be on the line the current line shifts to
+                const belowLineText: string = editor.getLine(belowLine); // Gets the text on the below line
+
+                editor.setLine(line, belowLineText); // Place the below line on the current line
+                editor.setLine(belowLine, lineText); // Place the current line on the below line
+                editor.setCursor(cursorPos); // Sets the cursor at its new position on the below line
+            } else {
+                return;
+            }
+        }
     }
-  
+
     isEditor() { // Check to see if we are in a markdown file editor
         const view = this.app.workspace.getActiveViewOfType(MarkdownView);
         
@@ -47,9 +76,5 @@ export default class Blocks extends Plugin { // The main plugin class
         }
         console.log('In MarkdownView') //REMOVE
         return true;
-    }   
-
-    moveLineUp() {
-
     }
 }
